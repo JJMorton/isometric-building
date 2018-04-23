dbcab0f97b417d5b59a86f8ba34c6457b9e003cd
Myeloon <jjmorton1636@gmail.com>
2018-04-23 22:52:22 +0100

    performance improvements

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -2,17 +2,28 @@
 
 	'use strict';
 
-	// Set up canvas element and rendering context
+	/*
+		Set up canvas element and rendering context
+	*/
 	const canvas = document.querySelector('canvas');
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	const ctx = canvas.getContext('2d');
 
+	/*
+		Buffer canvas to draw grid to
+		Only update tiles that have changed
+	*/
+	const buffer = document.createElement('canvas');
+	buffer.width = canvas.width;
+	buffer.height = canvas.height;
+	const bufferCtx = buffer.getContext('2d');
 
 
-	// ################################################################################
-	// #                          USER VARIABLES + GAME DATA                          #
-	// ################################################################################
+
+	/*
+		USER VARIABLES + GAME DATA
+	*/
 
 
 	window.Canvas = {
@@ -57,15 +68,16 @@
 			{ top: "#BBBB00", front: "#999900", side: "#AAAA00" }, // Yellow
 			{ top: "#338800", front: "#115500", side: "#216500" }, // Green
 			{ top: "#11AAAA", front: "#008888", side: "#059999" }  // Turquoise
-		]
+		],
+		unrendered: true
 
 	};
 
 
 
-	// ################################################################################
-	// #                        TILE RENDERING + CALCULATIONS                         #
-	// ################################################################################
+	/*
+		TILE RENDERING + CALCULATIONS
+	*/
 
 
 	const isOnTile = (p, tile, tileSize) => {
@@ -115,37 +127,49 @@
 
 			// If on the first row, create new row above
 			if (y === 0) {
-				Canvas.grid.splice(0, 0, [-1]);
+				Canvas.grid.splice(0, 0, [{type: -1}]);
+				Canvas.unrendered = true;
 				// The original tile will now have a larger y coordinate
 				y++;
 				Canvas.options.offset.x += 1;
 				Canvas.options.offset.y -= 0.5;
 			}
 			// Fill all tiles up to the neighbour
-			while (Canvas.grid[y - 1].length <= x) Canvas.grid[y - 1].push(-1);
+			while (Canvas.grid[y - 1].length <= x) {
+				Canvas.grid[y - 1].push({type: -1});
+				Canvas.unrendered = true;
+			}
 
 
 			// Tile to the right
 
 			// If on the end of the row, create tile to the right
-			if (x === Canvas.grid[y].length - 1) Canvas.grid[y].push(-1);
+			if (x === Canvas.grid[y].length - 1) {
+				Canvas.grid[y].push({type: -1});
+				Canvas.unrendered = true;
+			}
 
 
 			// Tile below
 
 			// If on the last row, create new row below
 			if (y === Canvas.grid.length - 1) {
-				Canvas.grid.push([-1]);
+				Canvas.grid.push([{type: -1}]);
+				Canvas.unrendered = true;
 			}
 			// Fill all tiles up to the neighbour
-			while (Canvas.grid[y + 1].length <= x) Canvas.grid[y + 1].push(-1);
+			while (Canvas.grid[y + 1].length <= x) {
+				Canvas.grid[y + 1].push({type: -1});
+				Canvas.unrendered = true;
+			}
 
 
 			// Tile to the left
 
 			// If at the beginning of a row, create column to the left
 			if (x === 0) {
-				Canvas.grid.forEach(row => row.splice(0, 0, -1));
+				Canvas.grid.forEach(row => row.splice(0, 0, {type: -1}));
+				Canvas.unrendered = true;
 				// The original tile will have a larger x coordinate
 				x++;
 				Canvas.options.offset.x -= 1;
@@ -170,29 +194,35 @@
 		// Adds a tile to the grid and updates neighbour states
 
 		// Can't add tile if it's already there
-		if (Canvas.grid[y][x] === type) return;
+		if (Canvas.grid[y][x] && Canvas.grid[y][x].type === type) return;
 
-		if (!Canvas.grid[y][x] || Canvas.grid[y][x] <= 0) {
+		if (!Canvas.grid[y][x] || Canvas.grid[y][x].type <= 0) {
 
 			// Add new tile
 
-			while (Canvas.grid.length <= y) Canvas.grid.push([-1]);
-			while (Canvas.grid[y].length <= x) Canvas.grid[y].push(-1);
+			while (Canvas.grid.length <= y) Canvas.grid.push([{type: -1}]);
+			while (Canvas.grid[y].length <= x) Canvas.grid[y].push({type: -1});
 
-			Canvas.grid[y][x] = type;
+			Canvas.grid[y][x].type = type;
 			Canvas.tileCount++;
 
 			getNeighbours(x, y, true).forEach(t => {
-				if (Canvas.grid[t.y][t.x] === -1) Canvas.grid[t.y][t.x] = 0;
+				if (Canvas.grid[t.y][t.x].type === -1) {
+					Canvas.grid[t.y][t.x].type = 0;
+					Canvas.unrendered = true;
+				}
 			});
 
 		} else {
 
 			// Change tile texture
 
-			if (Canvas.grid[y][x] > 0) Canvas.grid[y][x] = type;
+			if (Canvas.grid[y][x].type > 0) Canvas.grid[y][x].type = type;
 
 		}
+
+		// Mark tile for rendering
+		Canvas.unrendered = true;
 
 	};
 
@@ -201,14 +231,15 @@
 		// Removes a tile from the grid and updates neighbour states
 
 		// Can't remove an empty tile
-		if (Canvas.grid[y][x] <= 0) return;
+		if (Canvas.grid[y][x].type < 1) return;
 
-		Canvas.grid[y][x] = 0;
+		Canvas.grid[y][x].type = 0;
+		Canvas.unrendered = true;
 		Canvas.tileCount--;
 
 		// Remove any trailing empty spaces from the row
 		const removeTrailing = y => {
-			while (Canvas.grid[y][Canvas.grid[y].length - 1] === -1) Canvas.grid[y].pop();
+			while (Canvas.grid[y].length > 0 && Canvas.grid[y][Canvas.grid[y].length - 1].type === -1) Canvas.grid[y].pop();
 		};
 
 		// Returns true if the tile has at least one neighbour
@@ -216,24 +247,24 @@
 			const neighbours = getNeighbours(x, y, false);
 			const total = neighbours.reduce((sum, n) => {
 				if (!Canvas.grid[n.y] || !Canvas.grid[n.y][n.x]) return sum;
-				return sum + Math.max(Canvas.grid[n.y][n.x], 0);
+				return sum + Math.max(Canvas.grid[n.y][n.x].type, 0);
 			}, 0);
 
 			// If the total is 0, all the neighbour tiles are empty
 			return total === 0;
 		};
 
-		// If any of the neighbours now has no neighbours, remove them
+		// If any of the neighbours now have no neighbours and doesn't have a tile, remove it
 		getNeighbours(x, y, false).forEach(t => {
-			if (Canvas.grid.length <= t.y || Canvas.grid[t.y].length <= t.x || Canvas.grid[t.y][t.x] !== 0) return;
+			if (Canvas.grid.length <= t.y || Canvas.grid[t.y].length <= t.x || Canvas.grid[t.y][t.x].type !== 0) return;
 			if (noNeighbours(t.x, t.y)) {
-				Canvas.grid[t.y][t.x] = -1;
+				Canvas.grid[t.y][t.x].type = -1;
 				removeTrailing(t.y);
 			}
 		});
 
 		// If the tile has no neighbours, remove it
-		if (noNeighbours(x, y)) Canvas.grid[y][x] = -1;
+		if (noNeighbours(x, y)) Canvas.grid[y][x].type = -1;
 
 		// Remove empty tiles after deleted one
 		removeTrailing(y);
@@ -245,8 +276,8 @@
 		}
 
 		// Remove empty rows from start and end
-		while (Canvas.grid[Canvas.grid.length - 1].every(x => x === -1)) Canvas.grid.pop();
-		while (Canvas.grid[0].every(x => x === -1)) {
+		while (Canvas.grid[Canvas.grid.length - 1].every(x => x.type === -1)) Canvas.grid.pop();
+		while (Canvas.grid[0].every(x => x.type === -1)) {
 			Canvas.grid.shift();
 			Canvas.options.offset.add(-1, 0.5);
 		}
@@ -258,18 +289,18 @@
 
 
 
-	// ################################################################################
-	// #                                ANIMATION LOOP                                #
-	// ################################################################################
+	/*
+		ANIMATION LOOP
+	*/
 
 
 	const animate = () => {
 
-		const currentTime = Date.now();
+		const currentTime = performance.now();
 		Canvas.delta = currentTime - Canvas.prevTime;
 		Canvas.prevTime = currentTime;
 
-		Canvas.fpsElt.innerHTML = Math.floor(1000 / Canvas.delta);
+		if (Canvas.frameCount % 4 === 0) Canvas.fpsElt.innerHTML = Math.floor(1000 / Canvas.delta);
 
 		// Either the height of the grid or the longest row
 		const gridSize = Math.max(Canvas.grid.length, Canvas.grid.map(x => x).sort((a, b) => b.length - a.length)[0].length);
@@ -280,26 +311,22 @@
 		// Height of tiles
 		const height = tileSize * 0.5;
 
-		// Zoom controls
-		if (Canvas.keys.find(k => k.name === "up").pressed) {
-			Canvas.options.zoom += 0.05 * Canvas.options.zoom;
-		}
-		if (Canvas.keys.find(k => k.name === "down").pressed) {
-			Canvas.options.zoom -= 0.05 * Canvas.options.zoom;
-		}
-
 		// Move controls
 		if (Canvas.keys.find(k => k.name === "w").pressed) {
 			Canvas.options.offset.y += 0.02 * screenSize / tileSize;
+			Canvas.unrendered = true;
 		}
 		if (Canvas.keys.find(k => k.name === "a").pressed) {
 			Canvas.options.offset.x += 0.02 * screenSize / tileSize;
+			Canvas.unrendered = true;
 		}
 		if (Canvas.keys.find(k => k.name === "s").pressed) {
 			Canvas.options.offset.y -= 0.02 * screenSize / tileSize;
+			Canvas.unrendered = true;
 		}
 		if (Canvas.keys.find(k => k.name === "d").pressed) {
 			Canvas.options.offset.x -= 0.02 * screenSize / tileSize;
+			Canvas.unrendered = true;
 		}
 
 		// Texture changing key state
@@ -312,104 +339,134 @@
 		// Store position of selected tile
 		let selected;
 
+		if (Canvas.unrendered) {
+			bufferCtx.fillStyle = Canvas.bgColor;
+			bufferCtx.fillRect(0, 0, buffer.width, buffer.height);
+		}
+
 		Canvas.grid.forEach((row, y) => {
 			row.forEach((_, x) => {
 
-				if (row[x] === -1) return;
+				if (row[x].type === -1) return;
 
+				// Calculate position to draw the tile
 				const position = Vector.toIsometric(x * tileSize, y * tileSize);
 				position.set(0.5 * canvas.width + position.x + Canvas.options.offset.x * tileSize, 0.5 * canvas.height + position.y + Canvas.options.offset.y * tileSize);
+
+				// Calculate if the user has selected the tile
 				const mouseIsOn = isOnTile(Canvas.mouse.position, position, tileSize);
-				if (mouseIsOn) selected = new Vector(x, y);
+				if (mouseIsOn) selected = {index: new Vector(x, y), position: position};
 
 				// Only draw tiles that are visible
-				if (!(row[x] === 0 || position.x < -tileSize || position.x > canvas.width + tileSize || position.y < -tileSize - height || position.y > canvas.height + tileSize/2)) {
+				if (Canvas.unrendered || row[x].unrendered) {
 
-					// Render top
-					let stroke = "#00000000";
-					let fill = Canvas.textures[row[x] - 1].top;
-					if (Canvas.options.editMode === 0) {
-						stroke = fill;
-					}
-					drawTop(ctx, position, tileSize, fill, stroke);
+					row[x].unrendered = false;
 
-					// Render front
-					if (y === Canvas.grid.length - 1 || x >= Canvas.grid[y + 1].length || Canvas.grid[y + 1][x] <= 0) {
-						ctx.fillStyle = Canvas.textures[row[x] - 1].front;
-						ctx.strokeStyle = ctx.fillStyle;
-						ctx.beginPath();
-						ctx.moveTo(position.x - tileSize, position.y);
-						ctx.lineTo(position.x - tileSize, position.y + height);
-						ctx.lineTo(position.x, position.y + tileSize * 0.5 + height);
-						ctx.lineTo(position.x, position.y + tileSize * 0.5);
-						ctx.closePath();
-						ctx.fill();
-						if (Canvas.options.editMode === 0) ctx.stroke();
-					}
+					if (!(row[x].type === 0 ||
+						position.x < -tileSize ||
+						position.x > canvas.width + tileSize ||
+						position.y < -tileSize - height ||
+						position.y > canvas.height + tileSize/2)) {
 
-					// Render side
-					if (x === row.length - 1 || row[x + 1] <= 0) {
-						ctx.fillStyle = Canvas.textures[row[x] - 1].side;
-						ctx.strokeStyle = ctx.fillStyle;
-						ctx.beginPath();
-						ctx.moveTo(position.x + tileSize, position.y);
-						ctx.lineTo(position.x + tileSize, position.y + height);
-						ctx.lineTo(position.x, position.y + tileSize * 0.5 + height);
-						ctx.lineTo(position.x, position.y + tileSize * 0.5);
-						ctx.closePath();
-						ctx.fill();
-						if (Canvas.options.editMode === 0) ctx.stroke();
-					}
-				}
+						// Render top
+						let stroke = "#00000000";
+						let fill = Canvas.textures[row[x].type - 1].top;
+						if (Canvas.options.editMode === 0) {
+							stroke = fill;
+						}
+						drawTop(bufferCtx, position, tileSize, fill, stroke);
 
-				// If editing, draw grid lines around tiles
-				if (Canvas.options.editMode > 0 && mouseIsOn && (Canvas.options.editMode !== 2 || row[x] > 0)) {
-					// Overlay on currently selected tile
-					let fill = Math.floor(Math.sin(Math.PI * Canvas.frameCount / 30) * 125 + 125).toString(16);
-					if (fill.length === 1) fill = "0" + fill;
-					let stroke = Math.floor(Math.cos(Math.PI * Canvas.frameCount / 30) * 125 + 125).toString(16);
-					if (stroke.length === 1) stroke = "0" + stroke;
-					drawTop(ctx, position, tileSize, "#" + fill + fill + fill + "66", "#" + stroke + stroke + stroke);
+						// Render front
+						if (y === Canvas.grid.length - 1 || x >= Canvas.grid[y + 1].length || Canvas.grid[y + 1][x].type <= 0) {
+							bufferCtx.fillStyle = Canvas.textures[row[x].type - 1].front;
+							bufferCtx.strokeStyle = bufferCtx.fillStyle;
+							bufferCtx.beginPath();
+							bufferCtx.moveTo(position.x - tileSize, position.y);
+							bufferCtx.lineTo(position.x - tileSize, position.y + height);
+							bufferCtx.lineTo(position.x, position.y + tileSize * 0.5 + height);
+							bufferCtx.lineTo(position.x, position.y + tileSize * 0.5);
+							bufferCtx.closePath();
+							bufferCtx.fill();
+							if (Canvas.options.editMode === 0) bufferCtx.stroke();
+						}
 
-					// Change the texture of the tile if q is pressed
-					if (changeTexture && row[x] > 0) {
-						Canvas.keys.find(k => k.name === "q").pressed = false;
-						row[x]++;
-						if (row[x] > Canvas.textures.length) row[x] = 1;
+						// Render side
+						if (x === row.length - 1 || row[x + 1].type <= 0) {
+							bufferCtx.fillStyle = Canvas.textures[row[x].type - 1].side;
+							bufferCtx.strokeStyle = bufferCtx.fillStyle;
+							bufferCtx.beginPath();
+							bufferCtx.moveTo(position.x + tileSize, position.y);
+							bufferCtx.lineTo(position.x + tileSize, position.y + height);
+							bufferCtx.lineTo(position.x, position.y + tileSize * 0.5 + height);
+							bufferCtx.lineTo(position.x, position.y + tileSize * 0.5);
+							bufferCtx.closePath();
+							bufferCtx.fill();
+							if (Canvas.options.editMode === 0) bufferCtx.stroke();
+						}
+
 					}
 
-				} else if (Canvas.options.editMode > 0 && (Canvas.options.editMode !== 2 || row[x] > 0)) {
-					// White outline around all other tiles
-					drawTop(ctx, position, tileSize, "#00000000", "#FFFFFF66");
+					// If editing, draw grid lines around tiles
+					if (Canvas.options.editMode > 0 && (Canvas.options.editMode !== 2 || row[x].type > 0)) {
+						// White outline around all other tiles
+						drawTop(bufferCtx, position, tileSize, "#00000000", "#FFFFFF66");
+					}
+
 				}
 
 			});
 		});
 
+		// Mark canvas as rendered
+		Canvas.unrendered = false;
+
+		// Draw buffer to canvas
+		ctx.drawImage(buffer, 0, 0);
+
 		if (selected) {
+
+			const selectedTile = Canvas.grid[selected.index.y][selected.index.x];
+
+			if (Canvas.options.editMode > 0 && (Canvas.options.editMode !== 2 || selectedTile.type > 0)) {
+				// Overlay on currently selected tile
+				let fill = Math.floor(Math.sin(Math.PI * Canvas.frameCount / 30) * 125 + 125).toString(16);
+				if (fill.length === 1) fill = "0" + fill;
+				let stroke = Math.floor(Math.cos(Math.PI * Canvas.frameCount / 30) * 125 + 125).toString(16);
+				if (stroke.length === 1) stroke = "0" + stroke;
+				drawTop(ctx, selected.position, tileSize, "#" + fill + fill + fill + "66", "#" + stroke + stroke + stroke);
+
+				// Change the texture of the tile if q is pressed
+				if (changeTexture && selectedTile.type > 0) {
+					Canvas.keys.find(k => k.name === "q").pressed = false;
+					selectedTile.type++;
+					if (selectedTile.type > Canvas.textures.length) selectedTile.type = 1;
+				}
+
+			}
+
 			if (Canvas.options.editMode === 1) {
 
 				// Building actions
 
-				if (Canvas.mouse.right && Canvas.grid[selected.y][selected.x] > 0) {
-					removeTile(selected.x, selected.y);
+				if (Canvas.mouse.right && selectedTile.type > 0) {
+					removeTile(selected.index.x, selected.index.y);
 				}
-				if (Canvas.mouse.left && Canvas.grid[selected.y][selected.x] === 0) {
-					addTile(selected.x, selected.y, 1);
+				if (Canvas.mouse.left && selectedTile.type === 0) {
+					addTile(selected.index.x, selected.index.y, 1);
 				}
 
 			} else if (Canvas.options.editMode === 2) {
 
 				// Painting actions
 
-				if (Canvas.mouse.left && Canvas.grid[selected.y][selected.x] > 0) {
-					addTile(selected.x, selected.y, Canvas.options.textureToUse);
+				if (Canvas.mouse.left && selectedTile.type > 0) {
+					addTile(selected.index.x, selected.index.y, Canvas.options.textureToUse);
 				}
 
 			}
 		}
 
-		Canvas.deltaElt.innerHTML = Date.now() - currentTime;
+		if (Canvas.frameCount % 4 === 0) Canvas.deltaElt.innerHTML = Math.floor((performance.now() - currentTime) * 10) / 10;
 		Canvas.frameCount++;
 
 		requestAnimationFrame(animate);
@@ -420,9 +477,9 @@
 
 
 
-	// ################################################################################
-	// #                       EVENT LISTENERS / DOM MANAGEMENT                       #
-	// ################################################################################
+	/*
+		EVENT LISTENERS / DOM MANAGEMENT
+	*/
 
 
 	// Add all the textures to the paints container
@@ -461,6 +518,7 @@
 	const tools = document.getElementsByClassName("tool");
 	for (let i = 0; i < tools.length; i++) {
 		tools[i].addEventListener("click", () => {
+			Canvas.unrendered = true;
 			Canvas.options.editMode = i;
 			document.getElementsByClassName("tool enabled")[0].className = "tool disabled";
 			tools[i].className = "tool enabled";
@@ -512,8 +570,10 @@
 		// Size of each cell from top to bottom (double the width)
 		const tileSize = Canvas.options.zoom * 100;
 
+		if (e.deltaY < 0 && Canvas.options.zoom * 100 > Math.min(canvas.width, canvas.height)) return;
 		const zoomAmount = e.deltaY * -0.0005 * Canvas.options.zoom;
 		Canvas.options.zoom += zoomAmount;
+		Canvas.unrendered = true;
 
 	});
 
@@ -532,7 +592,9 @@
 	window.addEventListener('resize', () => {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
-		ctx.fillStyle = Canvas.bgColor;
+		buffer.width = canvas.width;
+		buffer.height = canvas.height;
+		Canvas.unrendered = true;
 	});
 
 }());
